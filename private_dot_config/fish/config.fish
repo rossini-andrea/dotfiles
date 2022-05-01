d535143100af42d8e779e9e8f7b60a6eba8d80c9
Andrea Rossini <rossini.andrea.lc@gmail.com>
2022-05-01 22:29:19 +0200

    Added kakoune as editor, sxiv wrapper alias to fish.

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -11,10 +11,14 @@ end
 # PATH
 set PATH $PATH $HOME/bin $HOME/.local/bin
 
+set -x EDITOR ~/bin/kakwrapper
+set -x VISUAL $EDITOR
+
 # Aliases
 
 alias ls "ls --color=auto"
 alias code "codium"
+alias gallery "sxiv -r ."
 alias st-term "pyserial-miniterm --eol LF -e /dev/ttyACM0 115200"
 
 # Prompt
