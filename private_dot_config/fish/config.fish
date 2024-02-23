af82b76a91017f172a2edea911b9a67fc9712627
Andrea Rossini <rossini.andrea.lc@gmail.com>
2024-02-23 09:08:20 +0100

    Made gitlog cool with fzf.

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,6 +1,7 @@
 # Functions
 function fish_greeting
     begin
+        clear
         date
         figlet '    WELCOME !'
         echo (fish --version)\t\t(cat /proc/meminfo | grep MemAvailable | cut -c 14-) free
@@ -9,6 +10,25 @@ end
 
 function swappiness
     sudo sysctl -w vm.swappiness=$argv
+end
+
+function gitshow
+    set -l commit (echo $argv | sed -r -E 's/^[ \\\/\|\*]*([0-9a-f]*) .*$/\1/g')
+    #set -l pager_command "/usr/bin/fzf --ansi --no-sort --layout=reverse-list"
+    set -l pager_command "/usr/bin/nvim -R"
+    set -lx GIT_PAGER $pager_command
+    git show --no-color $commit
+end
+
+function gitlog
+    set -l pager_command "/usr/bin/fzf --ansi --no-sort --layout=reverse-list"
+    set -l show_command "--bind \"enter:execute(gitshow {})\""
+
+    set -lx GIT_PAGER $pager_command" "$show_command
+    git log \
+        --pretty=format:'%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(yellow)%d%C(reset)' \
+        --date=iso \
+        --graph --decorate --all --full-history
 end
 
 # PATH
@@ -22,7 +42,6 @@ alias ls "ls --color=auto"
 alias code "codium"
 alias gallery "sxiv -r ."
 alias st-term "pyserial-miniterm --eol LF -e /dev/ttyACM0 115200"
-alias gitlog "git log --pretty=format:'%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(yellow)%d%C(reset)' --graph --decorate --all --full-history"
 
 # Prompt
 starship init fish | source
