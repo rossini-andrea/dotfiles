8da1d0dedb76a9445bdc0addf13dd00005159962
Andrea Rossini <rossini.andrea.lc@gmail.com>
2024-03-21 18:35:58 +0100

    Completion shortcuts.

---
+++ private_dot_config/nvim/after/plugin/completion.lua
@@ -0,0 +1,12 @@
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+    })
+})
