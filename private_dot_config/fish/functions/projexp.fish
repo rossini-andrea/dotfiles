b4e4719c673c81c08c988cc3384d38d49d9cd164
Andrea Rossini <rossini.andrea.lc@gmail.com>
2024-06-28 08:32:08 +0200

    Prevent projexp from getting closed.

--- private_dot_config/fish/functions/projexp.fish
+++ private_dot_config/fish/functions/projexp.fish
@@ -1,21 +1,23 @@
 function projexp -d "Chooses preferred projects form configurable lists. ~/.projexp.paths and ~/.projexp.pins"
-    begin
-        cat ~/.projexp.paths |
+    while true
         begin
-            while read -l line
-                echo (fish -c "path resolve "$line)/*/
-            end
-        end | xargs ls -d
-        cat ~/.projexp.pins |
-        begin
-            while read -l line
-                if test -n $line
-                    echo (fish -c "path resolve "$line)
+            cat ~/.projexp.paths |
+            begin
+                while read -l line
+                    echo (fish -c "path resolve "$line)/*/
+                end
+            end | xargs ls -d
+            cat ~/.projexp.pins |
+            begin
+                while read -l line
+                    if test -n $line
+                        echo (fish -c "path resolve "$line)
+                    end
                 end
             end
-        end
-    end | not-fuzf \
-        --header "Select a project to open a tmux session" \
-        --bind "enter:execute(tmuxhere {})"
+        end | not-fuzf \
+            --header "Select a project to open a tmux session" \
+            --bind "enter:execute(tmuxhere {})"
+    end
 end
 
